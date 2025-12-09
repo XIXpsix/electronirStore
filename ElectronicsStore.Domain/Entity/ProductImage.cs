1839a138d5fe43acc61289b5aed3dfbc8700ac9f
panameraxx <lebedevdima902@gmail.com>
2025-12-09 17:25:10 +0200

    ю

--- ElectronicsStore.Domain/Entity/ProductImage.cs
+++ ElectronicsStore.Domain/Entity/ProductImage.cs
@@ -1,15 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ElectronicsStore.Domain.Entity
 {
     public class ProductImage
     {
         public int Id { get; set; }
 
-        // Добавили '?' - теперь может быть пустым
-        public string? ImagePath { get; set; }
+        public string? ImagePath { get; set; } // Добавили '?'
 
         public int ProductId { get; set; }
-
-        // Добавили '?' - теперь может быть пустым
         public Product? Product { get; set; }
     }
 }
